1bf663a0b6c03e43dc916e3641797507f215a954
Vedant Sasane <vedant.sasane@wolkus.com>
2023-12-20 19:09:09 +0530

    Notification Service Extension Files added

---
+++ src/ios/NotificationService.h
@@ -0,0 +1,12 @@
+//
+//  NotificationService.h
+//  NotificationServiceExtension
+//
+//  Created by Vedant Sasane on 20/12/23.
+//
+
+#import <UserNotifications/UserNotifications.h>
+
+@interface NotificationService : UNNotificationServiceExtension
+
+@end
